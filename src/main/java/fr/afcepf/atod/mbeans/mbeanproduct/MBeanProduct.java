355f0f9d681620d82523ac33ddcce3e2d2c604a6
Nicolas <nicolastorero@gmail.com>
2016-09-14 01:07:02 +0200

    index page

--- src/main/java/fr/afcepf/atod/mbeans/mbeanproduct/MBeanProduct.java
+++ src/main/java/fr/afcepf/atod/mbeans/mbeanproduct/MBeanProduct.java
@@ -7,6 +7,10 @@ package fr.afcepf.atod.mbeans.mbeanproduct;
 
 import fr.afcepf.atod.business.product.api.IBuProduct;
 import fr.afcepf.atod.vin.data.exception.WineException;
+import fr.afcepf.atod.wine.entity.Product;
+
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
@@ -21,11 +25,16 @@ public class MBeanProduct {
     @ManagedProperty(value="#{buProduct}")
     private IBuProduct buProduct;
     private String nameProd;
+    private List<Product> promotedWinesList;
     
     public MBeanProduct() {
         nameProd = "";
     }
     
+    public void initIndex() throws WineException{
+    	promotedWinesList = buProduct.getPromotedProductsSelection();
+    }
+
     public String findByNameProduct() throws WineException {
         String str = null;
         if(!nameProd.equalsIgnoreCase("")) {
@@ -33,7 +42,7 @@ public class MBeanProduct {
         }
         return str;
     }
-    
+        
      // ----------- Getters && Setters ----------------//
 
     public String getNameProd() {
@@ -51,6 +60,15 @@ public class MBeanProduct {
     public void setBuProduct(IBuProduct buProduct) {
         this.buProduct = buProduct;
     }
+
+	public List<Product> getPromotedWinesList() {
+		return promotedWinesList;
+	}
+
+	public void setPromotedWinesList(List<Product> promotedWinesList) {
+		this.promotedWinesList = promotedWinesList;
+	}
     
+   
     
 }
