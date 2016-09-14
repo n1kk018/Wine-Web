2a7db56d2d8480b03298dee84173e5c61d221a2e
fen <fen@fen-HP>
2016-09-14 17:47:43 +0200

    check

--- src/main/java/fr/afcepf/atod/mbeans/mbeanorder/MBeanCartManagement.java
+++ src/main/java/fr/afcepf/atod/mbeans/mbeanorder/MBeanCartManagement.java
@@ -21,14 +21,17 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.event.ValueChangeEvent;
 
+import org.apache.log4j.Logger;
+
 /**
  *
  * @author ronan
  */
 @SessionScoped
-@ManagedBean
+@ManagedBean(name="mBeanCartManagement")
 public class MBeanCartManagement {
-
+	public static Logger log = Logger.getLogger(MBeanCartManagement.class);
+	
 	@ManagedProperty(value = "#{buCustomer}")
     private IBuCustomer buCustomer;
 	
@@ -44,44 +47,39 @@ public class MBeanCartManagement {
 	 * initialiser le panier pour faire le parcours panier/validation paiement/.
 	 *********************************************************
 	 */
-	private List<OrderDetail> shoppingCart;
-	private List<Product> orderDetail;
-	private int quantity = 1;
+	private List<OrderDetail> shoppingCart = new ArrayList<>();
+	private OrderDetail orderDetail1;
+	private OrderDetail orderDetail2;
 	private Double totalLine = 0.0;
 	private Double totalCart = 0.0;
 	private Product prodcut ;
 	private Double totalTransportFree;
 	private int totalQuantity=0;
-	
+	private int quantity = 1;
 	
 
 	public void initCart() {
 		
+		if(shoppingCart.size()==0){
+			ProductWine redWine = new ProductWine(null, "bourgogne", 200.0, "bourgogne", "bourgogne", null, null, null, 512);
+			ProductWine whiteWine = new ProductWine(null, "provence", 100.0, "provence", "provence", null, null, null, 512);
+//			orderDetail2 = new OrderDetail(null,3, null, redWine);
+			orderDetail1 = new OrderDetail(null, 2, null, whiteWine);
 		
-		ProductWine redWine = new ProductWine(null, "bourgogne", 200.0, "bourgogne", "bourgogne", null, null, null, 512);
-		ProductWine whiteWine = new ProductWine(null, "provence", 100.0, "provence", "provence", null, null, null, 512);
+		shoppingCart.add(orderDetail1);
+//		shoppingCart.add(orderDetail2);
+		}
 		
-		orderDetail.add(whiteWine);
-		orderDetail.add(redWine);
-		
-		shoppingCart.add((OrderDetail) orderDetail);
-		
-//		Customer customer = (Customer) buCustomer.findUserById(1);
-//		
-//		orderDetail1 = new OrderDetail(null, 1, shoppingCart, redWine);
-//		orderDetail2 = new OrderDetail(null, 3, shoppingCart, whiteWine);
-//		
-//		shoppingCart = new Order(1, new Date(), null,
-//	            1, customer,
-//	            PaymentInfo paymentInfo);
+//		quantity = orderDetail1.getQuantite();
+		log.info("*******************************init cart**************************************");
 	}
 
 	/**
 	 * remove item shopping cart
 	 *
 	 */
-	public void removeOrderTetail(OrderDetail orderLine) {
-	shoppingCart.remove(orderLine);
+	public void removeOrderTetail(OrderDetail o) {
+	shoppingCart.remove(o);
 	}
 
 	/**
@@ -89,18 +87,22 @@ public class MBeanCartManagement {
 	 *
 	 */
 
-	public void onChangeNombreProduit(ValueChangeEvent event) {
-		quantity = orderDetail.size() ;
-		quantity = (int) event.getNewValue();
+	public void updateBasket() {
+		log.info("*****************************************"+ quantity);
+		orderDetail1.setQuantite(quantity);
+		
+		calculerOrderLine();
+	
 	}
 
 	/**
 	 * calculer prix d'une ligne de commande
 	 */
-	public Double calculerOrderLine( int quantity) {
-		totalLine = prodcut.getPrice() * quantity;
+	public Double calculerOrderLine() {
+		totalLine = (orderDetail1.getProductOrdered().getPrice())*(orderDetail1.getQuantite());
+		log.info("*******************************orderline**************************************");
 		return totalLine;
-
+		
 	}
 
 	/**
@@ -109,7 +111,7 @@ public class MBeanCartManagement {
 	public Double calculerTotalOrder(int quantity) {
 
 		for (OrderDetail o : shoppingCart) {
-			totalCart = totalCart + o.getQuantite() * o.getProductOrdered().getPrice() + totalTransportFree;
+			totalCart = totalCart + o.getQuantite() * (o.getProductOrdered().getPrice() )+ totalTransportFree;
 		}
 
 		return totalCart;
@@ -118,8 +120,8 @@ public class MBeanCartManagement {
 	/**
 	 * calculer prix du frais transport
 	 */
-	public Double calculerTransportFree(int quantity) {
-		totalTransportFree = (double) (quantity * 2);
+	public Double calculerTransportFree() {
+		totalTransportFree = (double) (totalQuantity * 2);
 		return totalTransportFree;
 	}
 	
@@ -138,8 +140,17 @@ public class MBeanCartManagement {
 
 	// ---------------- Getters && Setters ------------ //
 	
+	
 	public List<OrderDetail> getShoppingCart() {
 		return shoppingCart;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
 	}
 
 	public IBuCustomer getBuCustomer() {
@@ -162,20 +173,22 @@ public class MBeanCartManagement {
 		this.shoppingCart = shoppingCart;
 	}
 
-	public List<Product> getOrderDetail() {
-		return orderDetail;
+
+
+	public OrderDetail getOrderDetail1() {
+		return orderDetail1;
 	}
 
-	public void setOrderDetail(List<Product> orderDetail) {
-		this.orderDetail = orderDetail;
+	public void setOrderDetail1(OrderDetail orderDetail1) {
+		this.orderDetail1 = orderDetail1;
 	}
 
-	public int getQuantity() {
-		return quantity;
+	public OrderDetail getOrderDetail2() {
+		return orderDetail2;
 	}
 
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
+	public void setOrderDetail2(OrderDetail orderDetail2) {
+		this.orderDetail2 = orderDetail2;
 	}
 
 	public Double getTotalLine() {
